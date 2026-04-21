ad5754dccf3e38b86be68a7ed0f98be37db918e4
Samueldamil <damilareedun.com>
2026-04-21 08:20:33 +0100

    Update notification message

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,13 +1,16 @@
 import { Navigate } from "react-router-dom";
+import { useNotification } from "../context/NotificationContext";
 import { useAuth } from "../context/AuthContext";
 import Loading from "./Loading";
 
 export default function ProtectedRoute({ children }) {
     const { token, loading } = useAuth();
+    const { notify } = useNotification();
 
     if (loading) return <Loading />;
 
     if (!token) {
+        notify("Please register or login first", "error");
         return <Navigate to="/login" replace />;
     }
 
